121736b732347a183541c9ccfc2a9757fe6361ea
Linh Truong <thlinh8392@outlook.com>
2020-06-04 08:02:51 +0700

    chore: initialise project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'angular-lifecycle';
+export class AppComponent implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
+  displayChild: boolean = true;
+
+  constructor() {
+    console.log('AppComponent:Constructor');
+  }
+
+  toggle() {
+    this.displayChild = !this.displayChild;
+  }
+
+  ngOnChanges() {
+    console.log('AppComponent:OnChanges');
+  }
+
+  ngOnInit() {
+    console.log('AppComponent:OnInit');
+  }
+
+  ngDoCheck() {
+    console.log('AppComponent:DoCheck');
+  }
+  ngAfterContentInit() {
+    console.log('AppComponent:AfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('AppComponent:AfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('AppComponent:AfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('AppComponent:AfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('AppComponent:OnDestroy');
+  }
 }
